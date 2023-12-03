4c331c9a0ba5c8abac8e09d9f86e7d81f8ba3ec1
streamich <streamich@gmail.com>
2023-12-03 02:43:13 +0100

    feat: 🎸 implement remaining facade methods

--- src/MonacoEditorFacade.ts
+++ src/MonacoEditorFacade.ts
@@ -1,40 +1,30 @@
 import type {SimpleChange, EditorFacade, Selection} from 'collaborative-editor';
 import type {StrApi} from 'json-joy/es2020/json-crdt';
-import type * as monaco from 'monaco-editor';
+import * as monaco from 'monaco-editor';
 
-// API:
-// model.getOffsetAt(pos)
-// model.getPositionAt(offset)
-// model.applyEdits([{ range, text: insert }])
-// model..onDidChangeContent(listener)
-// model.onWillDispose(listener)
-// editor.getSelection()
-// editor.onDidChangeCursorSelection(listener)
-// editor.getModel()
-// editor.setSelection(selection)
-// editor.deltaDecorations(oldDecorations, newDecorations)
 export class MonacoEditorFacade implements EditorFacade {
   public selection!: Selection;
   public onchange?: (changes: SimpleChange[] | void) => void;
   public onselection?: () => void;
 
   private readonly modelChangeDisposable: monaco.IDisposable;
+  private readonly selectionDisposable: monaco.IDisposable;
 
   constructor(
     protected readonly str: StrApi,
     protected readonly editor: monaco.editor.IStandaloneCodeEditor,
   ) {
     this.modelChangeDisposable = editor.onDidChangeModelContent((event) => {
-      const rawChanges = event.changes.sort(({rangeOffset: offset1}, {rangeOffset: offset2}) => offset2 - offset1);
+      const rawChanges = event.changes.sort((c1, c2) => c2.rangeOffset - c1.rangeOffset);
       const changes: SimpleChange[] = [];
       const length = rawChanges.length;
       for (let i = 0; i < length; i++) {
         const {rangeOffset, rangeLength, text} = rawChanges[i];
         changes.push([rangeOffset, rangeLength, text]);
       }
-      console.log(changes);
       this.onchange?.(changes);
     });
+    this.selectionDisposable = editor.onDidChangeCursorSelection(() => this.onchange?.());
   }
 
   public get(): string {
@@ -49,20 +39,53 @@ export class MonacoEditorFacade implements EditorFacade {
     this.editor.setValue(text);
   }
 
+  private getRange(offset: number, length: number): monaco.IRange {
+    const model = this.editor.getModel()!;
+    const start = model.getPositionAt(offset);
+    const end = length ? model.getPositionAt(offset + length) : start;
+    return new monaco.Range(start.lineNumber, start.column, end.lineNumber, end.column);
+  }
+
+  public ins(pos: number, text: string): void {
+    const range = this.getRange(pos, 0);
+    this.editor.getModel()?.applyEdits([{range, text}]);
+  }
+
+  public del(pos: number, length: number): void {
+    const range = this.getRange(pos, length);
+    this.editor.getModel()?.applyEdits([{range, text: ''}]);
+  }
+
   public getSelection(): [number, number, -1 | 0 | 1] | null {
-    console.log('getSelection');
-    return null;
+    const editor = this.editor;
+    const selection = editor.getSelection();
+    if (!selection) return null;
+    const model = editor.getModel();
+    if (!model) return null;
+    const start = model.getOffsetAt(selection.getSelectionStart());
+    const end = model.getOffsetAt(selection.getPosition());
+    const direction = selection.getDirection() === monaco.SelectionDirection.LTR ? 1 : -1;
+    return [start, end, direction];
   }
 
   public setSelection(start: number, end: number, direction: -1 | 0 | 1): void {
-
-  }
-
-  protected createChange(event: InputEvent): SimpleChange | undefined {
-    return undefined;
+    const editor = this.editor;
+    const model = editor.getModel();
+    if (!model) return;
+    let startPosition = model.getPositionAt(start)
+    let endPosition = model.getPositionAt(end);
+    if (direction === -1) [startPosition, endPosition] = [endPosition, startPosition];
+    const selection = new monaco.Selection(
+      startPosition.lineNumber,
+      startPosition.column,
+      endPosition.lineNumber,
+      endPosition.column,
+    );
+    editor.setSelection(selection);
   }
 
   public dispose(): void {
     this.modelChangeDisposable.dispose();
+    this.selectionDisposable.dispose();
   }
 }
