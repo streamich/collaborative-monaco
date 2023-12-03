d810baaa00455b2022e041fc0feeeb573412c885
streamich <streamich@gmail.com>
2023-12-03 03:06:06 +0100

    test: 💍 add Storybook tests

--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -10,14 +10,15 @@ interface EditorProps {
 
 const Editor: React.FC<EditorProps> = ({src = ''}) => {
   const divEl = React.useRef<HTMLDivElement>(null);
-  const [model] = React.useMemo(() => {
+  const editorRef = React.useRef<monaco.editor.IStandaloneCodeEditor>(null);
+  const [model, clone] = React.useMemo(() => {
     const model = Model.withLogicalClock();
     model.api.root({text: src});
     return [model, model.clone()];
   }, []);
   React.useEffect(() => {
     if (!divEl.current) return;
-    const editor = monaco.editor.create(divEl.current, {});
+    const editor = ((editorRef as any).current = monaco.editor.create(divEl.current, {}));
     const unbind = bind(model.api.str(['text']), editor, true);
     return () => {
       unbind();
@@ -25,9 +26,57 @@ const Editor: React.FC<EditorProps> = ({src = ''}) => {
   }, [model]);
   React.useSyncExternalStore(model.api.subscribe, () => model.tick);
 
+  const insert = (text: string, position?: number) => {
+    const editor = editorRef.current;
+    if (!editor) return;
+    const editorModel = editor.getModel();
+    if (!editorModel) return;
+    const value = editorModel.getValue();
+    const start = editorModel.getPositionAt(position ?? value.length);
+    const end = length ? editorModel.getPositionAt(position ?? value.length) : start;
+    const range = new monaco.Range(start.lineNumber, start.column, end.lineNumber, end.column);
+    editorModel.applyEdits([{range, text}]);
+  };
+
   return (
     <div>
       <div className="Editor" ref={divEl} style={{width: 800, height: 250, border: '1px solid #ddd'}} />
+      <div>
+        <button onClick={() => insert('!')}>Append "!" to editor</button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setTimeout(() => {
+              insert('?');
+            }, 2000);
+          }}
+        >
+          Append "?" to model after 2s
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setTimeout(() => {
+              insert('1. ', 0);
+            }, 2000);
+          }}
+        >
+          Prepend "1. " to model after 2s
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setTimeout(() => {
+              model.reset(clone);
+            }, 2000);
+          }}
+        >
+          RESET after 2s
+        </button>
+      </div>
       <pre style={{fontSize: '10px'}}>
         <code>{model.root + ''}</code>
       </pre>
