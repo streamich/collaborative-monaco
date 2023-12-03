2facc324cdb281498022d0468884383e44920fe1
streamich <streamich@gmail.com>
2023-12-03 02:48:02 +0100

    feat: 🎸 improve binding

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,5 @@ import type {StrApi} from 'json-joy/es2020/json-crdt';
 
 export const bind = (str: StrApi, editor: monaco.editor.IStandaloneCodeEditor, polling?: boolean): (() => void) => {
   const facade = new MonacoEditorFacade(editor);
-  const binding = new StrBinding(str, facade);
-  binding.syncFromModel();
-  binding.bind(polling);
-  return binding.unbind;
+  return StrBinding.bind(str, facade, polling);
 };
