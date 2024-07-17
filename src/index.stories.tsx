005d80fff8dc4618d596eef371374f870c4a07d9
Vadim Dalecky <streamich@gmail.com>
2024-07-17 20:21:05 +0200

    test: 💍 add more story options

--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Model} from 'json-joy/lib/json-crdt';
+import {Model, s} from 'json-joy/lib/json-crdt';
 import type {Meta, StoryObj} from '@storybook/react';
 import * as monaco from 'monaco-editor';
 import {bind} from '.';
@@ -12,14 +12,13 @@ const Editor: React.FC<EditorProps> = ({src = ''}) => {
   const divEl = React.useRef<HTMLDivElement>(null);
   const editorRef = React.useRef<monaco.editor.IStandaloneCodeEditor>(null);
   const [model, clone] = React.useMemo(() => {
-    const model = Model.withLogicalClock();
-    model.api.root({text: src});
+    const model = Model.create(s.str(src));
     return [model, model.clone()];
   }, []);
   React.useEffect(() => {
     if (!divEl.current) return;
     const editor = ((editorRef as any).current = monaco.editor.create(divEl.current, {}));
-    const unbind = bind(model.api.str(['text']), editor, true);
+    const unbind = bind(model.s.toApi(), editor, true);
     return () => {
       unbind();
     };
@@ -52,6 +51,18 @@ const Editor: React.FC<EditorProps> = ({src = ''}) => {
             }, 2000);
           }}
         >
+          Append "?" to editor after 2s
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setTimeout(() => {
+              const str = model.s.toApi()
+              str.ins(str.length(), '?');
+            }, 2000);
+          }}
+        >
           Append "?" to model after 2s
         </button>
       </div>
@@ -60,6 +71,18 @@ const Editor: React.FC<EditorProps> = ({src = ''}) => {
           onClick={() => {
             setTimeout(() => {
               insert('1. ', 0);
+            }, 2000);
+          }}
+        >
+          Prepend "1. " to editor after 2s
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setTimeout(() => {
+              const str = model.s.toApi()
+              str.ins(0, '1. ');
             }, 2000);
           }}
         >
