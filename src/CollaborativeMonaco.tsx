4ea4336c52addeead7f7ba4235ea93eddaa903d5
Vadim Dalecky <streamich@gmail.com>
2024-07-18 23:30:17 +0200

    style: 💄 run Prettier

--- src/CollaborativeMonaco.tsx
+++ src/CollaborativeMonaco.tsx
@@ -12,16 +12,29 @@ export interface CollaborativeMonacoProps extends React.HTMLAttributes<HTMLDivEl
   onEditor?: (editor: monaco.editor.IStandaloneCodeEditor) => void;
 }
 
-export const CollaborativeMonaco: React.FC<CollaborativeMonacoProps> = ({str, editor: _editor, options, override, onEditor, ...rest}) => {
+export const CollaborativeMonaco: React.FC<CollaborativeMonacoProps> = ({
+  str,
+  editor: _editor,
+  options,
+  override,
+  onEditor,
+  ...rest
+}) => {
   const divRef = React.useRef<HTMLDivElement>(null);
   const editorRef = React.useRef<monaco.editor.IStandaloneCodeEditor>(null);
 
   useIsomorphicLayoutEffect(() => {
     if (!divRef.current) return;
-    const editor = _editor ?? monaco.editor.create(divRef.current, options ?? {
-      value: str.view(),
-      language: 'javascript',
-    }, override);
+    const editor =
+      _editor ??
+      monaco.editor.create(
+        divRef.current,
+        options ?? {
+          value: str.view(),
+          language: 'javascript',
+        },
+        override,
+      );
     if (onEditor) onEditor(editor);
     (editorRef as any).current = editor;
     const unbind = bind(str, editor, true);
