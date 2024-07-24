5f34960337ffb39e3531363729080550e9f134de
streamich <streamich@gmail.com>
2024-07-24 23:13:58 +0200

    style: 💄 run Prettier

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import {StrBinding, type CollaborativeStr} from 'collaborative-editor';
 import {MonacoEditorFacade} from './MonacoEditorFacade';
 import type * as monaco from 'monaco-editor';
 
-export const bind = (str: () => CollaborativeStr, editor: monaco.editor.IStandaloneCodeEditor, polling?: boolean): (() => void) => {
+export const bind = (
+  str: () => CollaborativeStr,
+  editor: monaco.editor.IStandaloneCodeEditor,
+  polling?: boolean,
+): (() => void) => {
   const facade = new MonacoEditorFacade(editor);
   return StrBinding.bind(str, facade, polling);
 };
